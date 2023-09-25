f9b1660f4826dfef274714e2b0a9794c2e8452d7
AnaReis223 <113352512+AnaReis223@users.noreply.github.com>
2023-09-25 11:55:31 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -8,6 +8,7 @@ form.addEventListener("submit",(e) =>{
     const nome = form.inNome.value
     const nota1 = Number(form.inNota1.value)
     const nota2 = Number(form.inNota2.value)
+
     const media = (nota1 + nota2)/2
     resp1.innerText = `Média das Notas ${media.toFixed(2)}`
 
